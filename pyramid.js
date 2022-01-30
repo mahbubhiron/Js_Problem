e49a7e7deec4762e20b3f9cf8a622898ebc05741
Md. Mahbub hiron <mhalom19@gmail.com>
2022-01-31 03:09:38 +0600

    last update on 21/1/2021

---
+++ pyramid.js
@@ -0,0 +1,8 @@
+
+
+for(var i=0;i<5;i++){
+    for(var j=0;j<i+1;j++){
+        console.log(" ",i);
+    }
+    // console.log(" ");
+}
--- sumOfUpperTriangle.js
+++ sumOfUpperTriangle.js
@@ -1,13 +1,15 @@
 function generatePyramid() {
     var totalNumberofRows = 5;
     var output = '';
+    var sum=0;
     for (var i = 1; i <= totalNumberofRows; i++) {
         for (var j = 1; j <= i; j++) {
+            sum = sum + j;
             output += j + '  ';
         }
         console.log(output);
         output = '';
-    }
+    }console.log(sum);
 }
 
 generatePyramid();
